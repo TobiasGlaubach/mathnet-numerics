ad7e46d7cdcb6dfecacd94b15012d015709d71a1
Tobias Glaubach <tobias.glaubach@asinco.de>
2018-07-24 08:21:18 +0200

    WIP: first prototypes for the SISO and MIMO System Model interfaces

---
+++ src/Numerics/LtiSystems/IMimoSystemModel.cs
@@ -0,0 +1,26 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace MathNet.Numerics.LtiSystems
+{
+    public interface IMimoSystemModel
+    {
+        string[] InputNames { get; }
+        string[] OutputNames { get; }
+        bool IsStable();
+        IEnumerable<double[]> CalcResponse(IEnumerable<double[]> uVecs);
+        IEnumerable<Vector<double>> CalcResponse(IEnumerable<Vector<double>> uVecs);
+
+        IEnumerable<Vector<double>> Impulse(int nSteps);
+        IEnumerable<Vector<double>> Impulse();
+        IEnumerable<Complex[]> Bode(int nPoints = 100);
+        IEnumerable<Complex[]> Bode(int nPoints, out double[] omega_vec);
+        IEnumerable<Complex[]> Bode(double[] omega_vec);
+        IEnumerable<Complex[]> GetPoles();
+        IEnumerable<Complex[]> GetZeros();
+    }
+}
---
+++ src/Numerics/LtiSystems/ISisoSystemModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace MathNet.Numerics.LtiSystems
+{
+    public interface ISisoSystemModel
+    {
+        string Name { get; }
+        bool IsStable();
+        IEnumerable<double> CalcResponse(IEnumerable<double> x);
+        double[] CalcResponse(double[] x);
+        double[] Impulse(int nSteps);
+        double[] Impulse();
+        Complex[] Bode(int nPoints = 100);
+        Complex[] Bode(int nPoints, out double[] omega_vec);
+        Complex[] Bode(double[] omega_vec);
+        Complex[] GetPoles();
+        Complex[] GetZeros();
+
+    }
+}
